34c9d6d083839a843c10ecf5784f5a976683bdf0
srishtigupta2333 <srishtigupta2333@gmail.com>
2022-03-05 22:48:50 +0530

    Multiple Linear Regression

--- Model Development in R.R
+++ Model Development in R.R
@@ -13,5 +13,18 @@ new_depdelay <- data.frame(
   DepDelayMinutes = c(12, 19, 24))
 pred <- predict(linear_model, newdata = new_depdelay, interval = "confidence")
 pred
-
 ## checking
+mlr <- lm(ArrDelayMinutes ~ CarrierDelay + LateAircraftDelay, 
+          data = aa_delays)
+summary(mlr)
+CarrierDelay <- c(10, 20, 30)
+LateAircraftDelay <- c(20, 60, 30)
+new_multidelay <- data.frame(CarrierDelay, LateAircraftDelay)
+pred <- predict(mlr, newdata = new_multidelay, interval = "confidence")
+pred
+library(ggplot2)
+ggplot(aa_delays, aes(x= DepDelayMinutes, y = ArrDelayMinutes)) + geom_point() +
+  stat_smooth(method = "lm", col = "red")
+linear_model <- lm(ArrDelayMinutes ~ DepDelayMinutes,
+                   data = aa_delays)
+plot(linear_model)
